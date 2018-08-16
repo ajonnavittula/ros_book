bf3a3bc011125996c87564a275e407ac96fd1206
Ananth Jonnavittula <krishna.ananth3@gmail.com>
2018-08-16 15:49:23 -0400

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,18 @@
+# CMake generated Testfile for 
+# Source directory: /home/ananth/ros_book/src
+# Build directory: /home/ananth/ros_book/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(fetch_ros/fetch_calibration)
+subdirs(fetch_ros/fetch_description)
+subdirs(fetch_gazebo/fetch_gazebo_demo)
+subdirs(fetch_ros/fetch_maps)
+subdirs(stockroom_ws)
+subdirs(fetch_ros/fetch_navigation)
+subdirs(fetch_ros/fetch_teleop)
+subdirs(fetch_gazebo/fetch_gazebo)
+subdirs(fetch_ros/fetch_ikfast_plugin)
+subdirs(fetch_ros/fetch_depth_layer)
+subdirs(fetch_ros/fetch_moveit_config)
---
+++ build/fetch_gazebo/fetch_gazebo_demo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ananth/ros_book/src/fetch_gazebo/fetch_gazebo_demo
+# Build directory: /home/ananth/ros_book/build/fetch_gazebo/fetch_gazebo_demo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
